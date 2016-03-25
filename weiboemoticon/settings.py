0298dbf494a8b7e1333eb72fab1c2029b6b6ada1
maye9999 <maye9999@163.com>
2016-03-25 15:57:04 +0800

    change database

--- weiboemoticon/settings.py
+++ weiboemoticon/settings.py
@@ -12,6 +12,26 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import os.path   
+from os import environ  
+debug = not environ.get("APP_NAME", "")   
+if debug:
+#LOCAL 本地调试用，便于导出数据库,根据本地MYSQL数据库填写下面参数<----------------如果文件中出现中文，一定要在开始添加 #coding:utf-8  
+    MYSQL_DB = 'pythondjangotest'      
+    MYSQL_USER = 'root'   
+    MYSQL_PASS = '123456'   
+    MYSQL_HOST_M = '127.0.0.1'   
+    MYSQL_HOST_S = '127.0.0.1'   
+    MYSQL_PORT = '3306'   
+else:
+#SAE
+    import sae.const
+    MYSQL_DB = sae.const.MYSQL_DB   
+    MYSQL_USER = sae.const.MYSQL_USER   
+    MYSQL_PASS = sae.const.MYSQL_PASS   
+    MYSQL_HOST_M = sae.const.MYSQL_HOST   
+    MYSQL_HOST_S = sae.const.MYSQL_HOST_S   
+    MYSQL_PORT = sae.const.MYSQL_PORT  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -58,8 +78,12 @@ WSGI_APPLICATION = 'weiboemoticon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': MYSQL_DB,   
+        'USER': MYSQL_USER,   
+        'PASSWORD': MYSQL_PASS,   
+        'HOST': MYSQL_HOST_M,   
+        'PORT': MYSQL_PORT,
     }
 }
 
